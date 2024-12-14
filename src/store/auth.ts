cc3faea5ea29c8662af65e55c392982e549d9e8f
lakyulakyu <martieszhidan@gmail.com>
2024-12-14 08:48:10 +0700

    configuration elint

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -2,6 +2,6 @@ import { create } from 'zustand';
 
 export const useStore = create((set) => ({
   userLogin: null,
-  setUserLogin: (userLoginData:any) => set({ userLogin: userLoginData }),
+  setUserLogin: (userLoginData:string) => set({ userLogin: userLoginData }),
   setLogout: () => set({ userLogin: null }),
 }));
